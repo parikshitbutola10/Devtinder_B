67d08857268b7221324d8fc03a3cb8f714cf6865
parikshitbutola10 <140898139+parikshitbutola10@users.noreply.github.com>
2026-01-30 02:29:01 +0530

    final

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -53,7 +53,7 @@ authRouter.post("/login", async (req,res) => {
         expires: new Date(Date.now() + 8 * 3600000),
       });
       
-      res.send("login successful!!!");
+      res.send(user);
     } else {
       throw new Error("invalid credentials");
     }
